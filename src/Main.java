05985b189141541755362485db785f3077a605b6
daipeng <daipeng01@fenbi.com>
2021-11-03 22:16:18 +0800

    第一次提交

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author daipeng01
+ * @desc
+ * @date 2021/11/3 10:15 下午
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.err.println("hello");
+    }
+}
